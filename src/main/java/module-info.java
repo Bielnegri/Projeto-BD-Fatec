5bf3a8ce1eabca0971d46570ff955e3566ddc584
André Lamego <andreolamego@gmail.com>
2025-11-24 00:25:22 -0300

    adiciona dependência javafx.graphics e abre novo módulo adapters.ui no module-info.java para suporte à interface do usuário

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,8 +7,10 @@ module fatec.lanchoneteapp {
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
     requires jtds;
+    requires javafx.graphics;
 
     opens fatec.lanchoneteapp.run to javafx.fxml;
+    opens fatec.lanchoneteapp.adapters.ui to javafx.fxml;
     exports fatec.lanchoneteapp.run;
     exports fatec.lanchoneteapp.adapters.ui;
 }
